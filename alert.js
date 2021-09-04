e7314626406599f494816a817a1746680870a22c
unknown <imtiajahamed6161@gmail.com>
2021-09-04 12:40:56 -0700

    first commit

---
+++ alert.js
@@ -0,0 +1,23 @@
+console.log('i am here');
+// alert('Ma is coming open your book')
+const maComing = () => {
+    alert('Ammu is Coming. open your book')
+}
+
+const maHome = () => {
+    const shawon = confirm('Are You Going To Home');
+    console.log(shawon);
+    if(shawon === true){
+        alert('amake fee ta bikash kre de')
+    }
+    else{
+        console.log('DGM !!! dore giye more !!');
+    }
+}
+const aksName =() => {
+    const imtiaj = prompt('What Is Your Name')
+    if(imtiaj){
+        console.log(imtiaj);
+    }
+  
+}
---
+++ location.js
@@ -0,0 +1,9 @@
+
+const refreshPage = () => {
+      location.reload();
+
+ }
+const visitIA = () => {
+      location.assign('https://www.facebook.com/david.carry.980');
+ }
+        
